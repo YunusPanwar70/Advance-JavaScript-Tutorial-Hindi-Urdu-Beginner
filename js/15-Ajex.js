7f134ad3365b2c7c6128729e0c2a3fd08391aaf4
YunusPanwar <yunuspanwar78@gmail.com>
2023-12-07 09:56:16 +0530

    learn advance js

--- js/15-Ajex.js
+++ js/15-Ajex.js
@@ -1,11 +1,18 @@
+// document.createElement("table");
 document.getElementById("btn").addEventListener("click",
     function loadData() {
         let xhttp = new XMLHttpRequest();
+        xhttp.getResponseHeader("Content-type", "application/json")
+
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText);
+                let data1 = JSON.parse(this.responseText);
+                let print = document.getElementById("data")
+                let node = "<tr><td>" + data1[0].id + "</td><td>" + data1[0].name + "</td><td>" + data1[0].email + "</td><td>" + data1[0].address.city + "</td></tr>";
+                print.appendChild(node);
             }
         }
         xhttp.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
         xhttp.send();
-    })
+    });
